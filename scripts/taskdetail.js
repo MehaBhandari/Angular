4e47c051a08941fb2b3a8839f343cc09861af275
MayankGupta <MayankGupta688@Gmail.com>
2017-12-18 19:10:58 +0530

    First step completed

--- scripts/taskdetail.js
+++ scripts/taskdetail.js
@@ -1,5 +1,44 @@
 (function() {
-    var taskManagement = angular.module("taskManagement", [])
+    var taskManagement = angular.module("taskManagement", []);
+
+    taskManagement.controller("taskManagementController", function($scope, taskManagementFactory){
+        $scope.heading = "Task Management";
+        $scope.taskArray = taskManagementFactory.taskArray;
+        $scope.updateFunction = function(){
+            alert("Value updated");
+        };
+        $scope.deleteFunction = function(){
+            
+            alert("Value deleted");
+        };
+    });
+
+    taskManagement.factory("taskManagementFactory", function() {
+        var taskArray = [{
+            id: "10001",
+            creationDate: "20 Jan 2014"
+        },
+        {
+            id: "10002",
+            creationDate: "22 Jan 2014"
+        },
+        {
+            id: "10003",
+            creationDate: "23 Jan 2014"
+        },
+        {
+            id: "10004",
+            creationDate: "24 Jan 2014"
+        },
+        {
+            id: "10005",
+            creationDate: "25 Jan 2014"
+        }];
+
+        return {
+            taskArray: taskArray
+        }
+    });
     
    
 })();
