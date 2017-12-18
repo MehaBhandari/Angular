4b314d702ed12d93b5cdfe17cf3ccb99583ca677
MayankGupta <MayankGupta688@Gmail.com>
2017-12-18 15:58:39 +0530

    edited from app

--- scripts/Employee.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var empModule = angular.module("empModule", ["detailModule"])
-
-    empModule.controller("empBasicController", function($scope, detailModuleFactory, employerDetailService){
-        $scope.name = detailModuleFactory.name;
-        $scope.age = detailModuleFactory.age;
-    });
-
-})();
