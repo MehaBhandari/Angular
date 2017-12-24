aabfd2736de140261962a25453c4e89fc2ef6cb6
MayankGupta <MayankGupta688@Gmail.com>
2017-12-24 17:05:35 +0530

    Done

--- scripts/learnDirective.js
+++ scripts/learnDirective.js
@@ -1,13 +1,15 @@
 (function(){
     var learnDirective = angular.module("learnDirective", []);
-    learnDirective.controller("learnDirectiveController", function($scope){
+    learnDirective.controller("learnDirectiveController", function($scope,$rootScope){
         $scope.name = "Meha";
         $scope.age = 25;
         $scope.randomText = "Hey beautiful";
         $scope.emp = {
             name: "Mayank",
             age: 29
-        }
+        };
+        $rootScope.Company = "GlobalLogic";
+        $rootScope.rootScopeText = "Change RootScope here";
     });
     learnDirective.directive("demo1Directive", function() {
         return{
@@ -19,4 +21,26 @@
             template: `<input type="text" ng-model="randomText">`
         };
     });
+    learnDirective.directive("demo2Directive", function(){
+        return {
+            template: `Company is: {{Company}}<br><br>
+                        This is <b>Root Scope Text</b>: {{rootScopeText}}`
+        };
+    });
+    learnDirective.directive("demo3Directive", function(){
+        return {
+            template: `Hi there now the following is transcluded text: <br><br>
+                        <ng-transclude></ng-transclude> `,
+            transclude: "true"
+            
+        };
+    });
+    learnDirective.directive("demo4Directive", function(){
+        return {
+            template: `Using interpolation in transclude: <br><br>
+                        Here it is: <b>{{twoWayBindedText}}</b>`,
+            transclude: "true",
+        }
+    });
+
 })();
